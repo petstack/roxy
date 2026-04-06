047228176c0cda1301bddf064e69d8766af76645
Vasili Kamrakou <diolektor@gmail.com>
2026-04-07 02:26:38 +0400

    feat: PhpExecutor trait definition

--- src/executor/mod.rs
+++ src/executor/mod.rs
@@ -1,1 +1,14 @@
 pub mod fastcgi;
+
+use crate::protocol::{PhpCallResult, PhpDiscoverResponse, PhpRequest};
+
+pub trait PhpExecutor: Send + Sync {
+    fn execute(
+        &self,
+        request: &PhpRequest<'_>,
+    ) -> impl std::future::Future<Output = anyhow::Result<PhpCallResult>> + Send;
+
+    fn discover(
+        &self,
+    ) -> impl std::future::Future<Output = anyhow::Result<PhpDiscoverResponse>> + Send;
+}
